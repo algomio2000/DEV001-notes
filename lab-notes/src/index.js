68816aea262b764ba6789c0ce7dd0f6229ef7151
Silvana Gonzalez <algomio2000@gmail.com>
2023-02-19 18:00:27 -0500

    Ruoter

--- lab-notes/src/index.js
+++ lab-notes/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter,RouterProvider} from "react-ruoter-dom";
 
+const router = createBrowserRouter(
+  {
+    path:"/",
+    element:<h1>SingInn</h1>
+  },
+  {
+    path:"/Create",
+    element:<h1>Create</h1>
+  },
+  {
+    path:"/WallNotes",
+    element:<h1>WallNotes</h1>
+  }
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router= {router}/>
   </React.StrictMode>
 );
 
