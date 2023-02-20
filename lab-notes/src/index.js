a28e67617d47bf091859a1ff3fc6816cb4a3b279
Silvana Gonzalez <algomio2000@gmail.com>
2023-02-19 21:51:31 -0500

    instalando firebase

--- lab-notes/src/index.js
+++ lab-notes/src/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider} from "react-ruoter-dom";
-import Create from './components/Create';
+import { BrowserRouter,Routes, Route} from "react-ruoter-dom";
+//import Create from './components/Create';
 import SingInn from './components/SingInn';
-import WallNotes from './components/WallNotes';
-const router = createBrowserRouter(
-  {
-    path:"/",
-    element:<SingInn></SingInn>
-  },
-  {
-    path:"/Create",
-    element: <Create></Create>,
-  },
-  {
-    path:"/WallNotes",
-    element:<WallNotes></WallNotes>
-  }
+//import WallNotes from './components/WallNotes';
+ReactDOM.render(
+<BrowserRouter>
+<Routes>
+  <Route path="/" element= {<SingInn />} />
+</Routes>
+</BrowserRouter>,
+document.getElementById('root')
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router= {router}/>
-  </React.StrictMode>
-);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
