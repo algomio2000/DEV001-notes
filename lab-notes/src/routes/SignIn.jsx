45cece25491ec0d5fee8900c2c50db3548bc0995
silvana Gonzalez <algomio2000@gmail.com>
2023-02-28 14:09:15 -0500

    subiendo imagenes

--- lab-notes/src/routes/SignIn.jsx
+++ lab-notes/src/routes/SignIn.jsx
@@ -1,12 +1,11 @@
 //import { async } from "@firebase/util";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/firebase.js";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import logoGoogle from "../imagenes"
+import logoCalendario from "../imagenes"
 export default function SingIn() {
 
-    
+
     async function handleOnClick() {
         const googleProvider = new GoogleAuthProvider();
         await signInWhiteGoogle(googleProvider);
@@ -19,35 +18,21 @@ export default function SingIn() {
             console.error(error);
         }
     }
- //function BasicExample() {
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <Button variant="primary" type="submit" onClick={handleOnClick} >
-        GOOGLE
-      </Button>
-    </Form>
-  );
-//}
-
-
-  
-        
-}
+   
+        <>
+          <h1 className="welcome">Welcome to</h1>
+          <img className="logo" src={logoCalendario} alt="ORGANIZE ME" />
+          <h1 className="mainTitle">organizate para cumplir tus sueños</h1>
+          <button className="btn" onClick={handleOnClick}>
+            Signin with
+            <img
+              className="googleIcon"
+              src={logoGoogle}
+              alt="google logo"
+            />
+          </button>
+        </>
+      );
+    }
