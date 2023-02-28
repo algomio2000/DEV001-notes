523f6b94354e04e34e4310478323bc169179dc1a
silvana Gonzalez <algomio2000@gmail.com>
2023-02-28 12:07:07 -0500

    trabando con css

--- lab-notes/src/routes/SignIn.jsx
+++ lab-notes/src/routes/SignIn.jsx
@@ -1,8 +1,12 @@
 //import { async } from "@firebase/util";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/firebase.js";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function SingIn() {
+
+    
     async function handleOnClick() {
         const googleProvider = new GoogleAuthProvider();
         await signInWhiteGoogle(googleProvider);
@@ -15,9 +19,35 @@ export default function SingIn() {
             console.error(error);
         }
     }
+ //function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-    return (<div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      <Button variant="primary" type="submit" onClick={handleOnClick} >
+        GOOGLE
+      </Button>
+    </Form>
+  );
+//}
+
+
   
-        <button onClick={handleOnClick}>Login with google</button>
-    </div>);
+        
 }
